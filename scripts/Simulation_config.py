ba19342f2b61e4f11bb7e3b5f2b769e00a888d45
Lukas Moosbrugger <molu@fhv.at>
2024-10-14 14:13:00 +0000

    Add code to evaluate the simulation run.

--- scripts/Simulation_config.py
+++ scripts/Simulation_config.py
@@ -33,7 +33,7 @@ class TrainingHistory():
     _15_MONTH = 466      # <= Baseline
 
 class PredictionHistory():
-    _2_DAYS = 24  # <= Baseline
+    _1_DAY = 24  # <= Baseline
     _7_DAYS = 24*7
 
 class UsedModels():
@@ -54,32 +54,33 @@ Config_of_one_run = namedtuple('Config_of_one_run', run_settings)
 configs = [
     # Baseline
     Config_of_one_run(ModelSize.MEDIUM, DoPretraining.YES, DoTransferLearning.YES, Aggregation_Count._74_HOUSEHOLDS,
-            NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._2_DAYS, UsedModels.ALL, Epochs.DEFAULT),
+            NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._1_DAY, UsedModels.ALL, Epochs.DEFAULT),
     
     # Vary the model sizes
     Config_of_one_run(ModelSize.SMALL, DoPretraining.YES, DoTransferLearning.YES, Aggregation_Count._74_HOUSEHOLDS,
-            NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._2_DAYS, UsedModels.ALL, Epochs.DEFAULT),
+            NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._1_DAY, UsedModels.ALL, Epochs.DEFAULT),
     Config_of_one_run(ModelSize.LARGE, DoPretraining.YES, DoTransferLearning.YES, Aggregation_Count._74_HOUSEHOLDS,
-            NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._2_DAYS, UsedModels.ALL, Epochs.DEFAULT),
+            NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._1_DAY, UsedModels.ALL, Epochs.DEFAULT),
         
     # Vary the community sizes
     Config_of_one_run(ModelSize.MEDIUM, DoPretraining.YES, DoTransferLearning.YES, Aggregation_Count._37_HOUSEHOLDS,
-            NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._2_DAYS, UsedModels.ALL, Epochs.DEFAULT),
+            NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._1_DAY, UsedModels.ALL, Epochs.DEFAULT),
     Config_of_one_run(ModelSize.MEDIUM, DoPretraining.YES, DoTransferLearning.YES, Aggregation_Count._10_HOUSEHOLDS,
-            NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._2_DAYS, UsedModels.ALL, Epochs.DEFAULT),
+            NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._1_DAY, UsedModels.ALL, Epochs.DEFAULT),
     Config_of_one_run(ModelSize.MEDIUM, DoPretraining.YES, DoTransferLearning.YES, Aggregation_Count._1_HOUSEHOLD,
-            NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._2_DAYS, UsedModels.ALL, Epochs.DEFAULT),
+            NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._1_DAY, UsedModels.ALL, Epochs.DEFAULT),
 
     # Vary the train set size
     Config_of_one_run(ModelSize.MEDIUM, DoPretraining.YES, DoTransferLearning.YES, Aggregation_Count._74_HOUSEHOLDS,
-            NrOfComunities._20, TrainingHistory._12_MONTH, PredictionHistory._2_DAYS, UsedModels.ALL, Epochs.DEFAULT),
+            NrOfComunities._20, TrainingHistory._12_MONTH, PredictionHistory._1_DAY, UsedModels.ALL, Epochs.DEFAULT),
     Config_of_one_run(ModelSize.MEDIUM, DoPretraining.YES, DoTransferLearning.YES, Aggregation_Count._74_HOUSEHOLDS,
-            NrOfComunities._20, TrainingHistory._6_MONTH, PredictionHistory._2_DAYS, UsedModels.ALL, Epochs.DEFAULT),
+            NrOfComunities._20, TrainingHistory._6_MONTH, PredictionHistory._1_DAY, UsedModels.ALL, Epochs.DEFAULT),
     Config_of_one_run(ModelSize.MEDIUM, DoPretraining.YES, DoTransferLearning.YES, Aggregation_Count._74_HOUSEHOLDS,
-            NrOfComunities._20, TrainingHistory._2_MONTH, PredictionHistory._2_DAYS, UsedModels.ALL, Epochs.DEFAULT),
+            NrOfComunities._20, TrainingHistory._2_MONTH, PredictionHistory._1_DAY, UsedModels.ALL, Epochs.DEFAULT),
 
     # Vary the input length
     Config_of_one_run(ModelSize.MEDIUM, DoPretraining.YES, DoTransferLearning.YES, Aggregation_Count._74_HOUSEHOLDS,
             NrOfComunities._20, TrainingHistory._15_MONTH, PredictionHistory._7_DAYS, UsedModels.ALL, Epochs.DEFAULT),    
 ]
 #########################################################################################################################
+
