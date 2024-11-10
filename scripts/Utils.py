b980418fea3427c7b1639418fe08c91aa05b9d88
Lukas Moosbrugger <molu@fhv.at>
2024-11-10 07:12:57 +0000

    add boxplot.

--- scripts/Utils.py
+++ scripts/Utils.py
@@ -273,8 +273,9 @@ class Evaluate_Models:
                 if print_style == 'latex':
                     latex_string += f' & {median_test_sMAPE} ({siqr_test_sMAPE})'
                 elif print_style == 'pandas_df':
-                    result_dict['error'][config_id][model_type] = median_test_sMAPE
-                    result_dict['deviation'][config_id][model_type] = siqr_test_sMAPE
+                    result_dict['median'][config_id][model_type] = median_test_sMAPE
+                    result_dict['first quartil'][config_id][model_type] = np.percentile(test_sMAPE,25)
+                    result_dict['third quartil'][config_id][model_type] = np.percentile(test_sMAPE,75)
                 elif print_style == 'shell':
                     # Print the results of the current config and modeltype
                     print(f'    Model: {model_type}')
