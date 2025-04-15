ff0bb1202bd00b4f695d954282bdf91eabf0caae
Lukas Moosbrugger <molu@fhv.at>
2025-04-15 11:08:18 +0200

    Change KNN, to use more neighbors.

--- models/KNN.py
+++ models/KNN.py
@@ -1,3 +1,4 @@
+from sklearn.neighbors import KNeighborsRegressor
 import torch
 
 class KNN():
@@ -8,31 +9,40 @@ class KNN():
         self.Y_train = None
         self.num_of_features = num_of_features
     
+        # Configure the KNN model.
+        # This was done empirically during developement 
+        # phase to the values with best forecast error.
+        # 
+        self.k = 40
+        self.weights = 'distance'
+        
+    # Store the training data
+    #
     def train_model(self, X_train, Y_train):
-
-        # Store the training data as flattened tensors
-        self.X_train = X_train.view(X_train.shape[0], -1)  # Flatten X_train from (nr_of_batches, timesteps, features) to (nr_of_batches, timesteps * features)
-        self.Y_train = Y_train  # Y_train remains unchanged in shape (nr_of_days, timesteps, 1)
+        
+        self.X_train = X_train
+        self.Y_train = Y_train  
     
-    # Given an input x, find the closest neighbor from the training data X_train
+    # Given an input x, find the closest neighbors from the training data X_train
     # and return the corresponding Y_train.
     #
     def forward(self, x):
         
-        batch_size = x.size(0)
-        nr_of_timesteps = x.size(1)
-        x_flat = x.view(batch_size, -1)  # Flatten input to (batch_size, 24*num_of_features)
-        assert x_flat.shape == torch.Size([batch_size, nr_of_timesteps * self.num_of_features]), \
-            f"Shape mismatch: got {x_flat.shape}, expected ({batch_size}, {nr_of_timesteps * self.num_of_features})"
-        distances = torch.cdist(x_flat, self.X_train)  # Compute pairwise distances
-        assert distances.shape == torch.Size([batch_size, self.X_train.shape[0]]), \
-            f"Shape mismatch: got {distances.shape}, expected ({batch_size}, {self.Y_train.shape[0]})"
-        nearest_neighbors = torch.argmin(distances, dim=1)  # Get nearest neighbor index
-        assert nearest_neighbors.shape == torch.Size([batch_size]), \
-            f"Shape mismatch: got {nearest_neighbors.shape}, expected ({batch_size})"
-        y_pred = self.Y_train[nearest_neighbors]  # Fetch corresponding Y_train
-        assert y_pred.shape == torch.Size([batch_size, 24, 1]), \
-            f"Shape mismatch: got {y_pred.shape}, expected ({torch.Size([batch_size, 24, 1])})"
+        # Fit the model with hourly training data
+        #
+        knn = KNeighborsRegressor(n_neighbors = self.k, weights=self.weights)
+        batches, timesteps, num_features = self.X_train.shape
+        X_hourly = self.X_train.view(batches * timesteps, num_features)
+        Y_hourly = self.Y_train.view(batches * timesteps, 1)
+        knn.fit(X_hourly, Y_hourly)
+        
+        # Prediction on new hourly data
+        #
+        batches, timesteps, num_features = x.shape
+        x_hourly = x.view(batches * timesteps, num_features)
+        y_pred = knn.predict(x_hourly)
+        y_pred = torch.tensor(y_pred).view(batches, timesteps, 1)
+
         return y_pred
     
     def state_dict(self):
@@ -44,4 +54,3 @@ class KNN():
     def load_state_dict(self, state_dict):
         self.X_train = state_dict['X_train']
         self.Y_train = state_dict['Y_train']
-        
