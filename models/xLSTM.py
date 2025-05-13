21be32e4ae8b885f57267685e5c7e57f57134b4f
Lukas Moosbrugger <molu@fhv.at>
2025-05-13 07:51:23 +0200

    Add positional encoding to xLSTM, to improve comparability to the Transformer model.

--- models/xLSTM.py
+++ models/xLSTM.py
@@ -1,6 +1,7 @@
 
 import torch
 import torch.nn as nn
+import math
 
 from xlstm import (
     xLSTMBlockStack,
@@ -105,10 +106,41 @@ class xLSTM(nn.Module):
 
         # Adding none-xlstm layers
         self.input_projection = nn.Linear(num_of_features, d_model)
+        self.positional_encoding = PositionalEncoding(d_model, timesteps=self.forecast_horizon)
         self.output_layer = nn.Linear(d_model, 1)
         
     def forward(self, x):
         x = self.input_projection(x)
+        x = self.positional_encoding(x)
         x = self.xlstm_stack(x)
         x = self.output_layer(x)
+        return x
+
+# This implementation of positional encoding is based on the
+# "Attention Is All You Need" paper, and is conceptually similar to:
+# https://stackoverflow.com/questions/77444485/using-positional-encoding-in-pytorch
+#
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, timesteps=5000):
+        super(PositionalEncoding, self).__init__()
+
+        pe = torch.zeros(timesteps, d_model)  # [timesteps, d_model]
+        position = torch.arange(0, timesteps, dtype=torch.float).unsqueeze(1)  # [timesteps, 1]
+        _2i = torch.arange(0, d_model, 2).float()
+        div_term = torch.exp(_2i * (-math.log(10000.0) / d_model))  # [d_model/2]
+
+        pe[:, 0::2] = torch.sin(position * div_term)  # Apply sin to even indices in the array
+        pe[:, 1::2] = torch.cos(position * div_term)  # Apply cos to odd indices in the array
+
+        pe = pe.unsqueeze(0)  # [1, timesteps, d_model]
+        self.register_buffer('pe', pe)  # Save as a non-learnable buffer
+
+    def forward(self, x):
+        
+        batches, timesteps, features = x.shape
+        assert (self.pe.size(1) == timesteps), f"Expected timesteps: {self.pe.size(1)}, received timesteps: {timesteps}"
+        assert (self.pe.size(2) == features), f"Expected features: {self.pe.size(2)}, received features: {features}"
+        
+        x = x + self.pe # Add positional encoding
+        
         return x
