880130664b2d900d56db28822b672001a55d0002
Lukas Moosbrugger <molu@fhv.at>
2025-10-28 16:11:03 +0100

    Add machine learning unittest

--- tests/test_forecasting_models.py
+++ tests/test_forecasting_models.py
@@ -3,7 +3,7 @@ import torch
 from loadforecasting_models import Knn, Lstm, Transformer, xLstm, Persistence, Normalizer
 
 def test_model_prediction():
-    """Test if models can be trained and make predictions with dummy data. """
+    """Test if creating, training and prediction works without error. """
 
     for model_class in [Knn, Lstm, Transformer, xLstm, Persistence]:
 
@@ -32,3 +32,72 @@ def test_model_prediction():
         y_pred = my_model.predict(x_test)
 
         assert y_pred.shape == (90, 24, 1)
+
+
+def test_model_linear_prediction():
+    """
+    Test if models can learn and predict a simple linear trendr and 
+    produce the correct output shape.
+    """
+
+    for model_class in [Lstm, Transformer, xLstm,]:
+
+        for seq_len in [24, 24*4, 24*7]:
+
+            print(f'Testing linear prediction with {model_class.__name__}' + \
+                f' model and seq_len={seq_len}.')
+
+            # Define a simple linear relationship: y = 2 * x + 1
+            batch_size = 40
+            n_features = 3
+
+            # Input data: Sine Values
+            full_periods = 5
+            x_vals = torch.linspace(0, 1, batch_size * seq_len).reshape(batch_size, seq_len, 1)
+            x_vals = torch.sin(full_periods * 2 * torch.pi * x_vals)
+            noise = 0.1 * torch.randn(batch_size, seq_len, 1)
+            y_train = 2 * x_vals + 1 + noise
+
+            # Add noise features
+            x_train = torch.cat([x_vals, torch.randn(size=(batch_size, seq_len, n_features - 1))],
+                dim=2)
+
+            # Normalize
+            normalizer = Normalizer()
+            x_train = normalizer.normalize_x(x_train, training=True)
+            y_train = normalizer.normalize_y(y_train, training=True)
+
+            # Train the ML model
+            model_class: Type[Union[Lstm, Transformer, xLstm]]
+            my_model = model_class('5k', num_of_features=x_train.size(2), normalizer=normalizer)
+            my_model.train_model(x_train, y_train, epochs=100, verbose=2)
+
+            # Test input data: Sine Values
+            x_test_vals = torch.linspace(1, 2, batch_size * seq_len).reshape(batch_size, seq_len, 1)
+            x_test_vals = torch.sin(full_periods * 2 * torch.pi * x_test_vals)
+            y_test_true = 2 * x_test_vals + 1
+            x_test = torch.cat([x_test_vals, torch.randn(batch_size, seq_len,
+                n_features - 1)], dim=2)
+            x_test = normalizer.normalize_x(x_test, training=False)
+
+            # Normalize and predict
+            y_pred = my_model.predict(x_test)
+            y_pred = normalizer.de_normalize_y(y_pred)
+
+            # 1. Check output shape
+            assert y_pred.shape == (batch_size, seq_len, 1), \
+                f"Unexpected output shape: {y_pred.shape}"
+
+            # 2. Check relative accuracy
+            treshold = 20.0 # [%]
+            mae = torch.mean(torch.abs(y_pred - y_test_true))
+            mean_true = torch.mean(torch.abs(y_test_true))
+            n_mae = mae / mean_true
+            print(f"Relative error for {model_class.__name__}: {n_mae:.2%}")
+            assert n_mae < treshold/100., f"{model_class.__name__} too inaccurate: {n_mae:.2%}"
+
+    print("All models passed the linear prediction test.")
+
+
+if __name__ == "__main__":
+    test_model_linear_prediction()
