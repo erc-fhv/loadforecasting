1fd2c4608396f6f8ba7fb79a22c7021d4abc29cb
Lukas Moosbrugger <molu@fhv.at>
2025-10-29 08:24:57 +0100

    add unittest for transfer learning

--- tests/test_forecasting_models.py
+++ tests/test_forecasting_models.py
@@ -106,6 +106,76 @@ def test_model_linear_prediction():
     print("All models passed the linear prediction test.")
 
 
+def test_transfer_learning():
+    """
+    Test if transfer learning works
+    """
+
+    # Define a simple linear relationship: y = 2 * x + 1
+    batch_size = 40
+    n_features = 3
+    seq_len = 24
+
+    # Input data: Sine Values
+    full_periods = 5
+    x_vals = torch.linspace(0, 1, batch_size * seq_len).reshape(batch_size, seq_len, 1)
+    x_vals = torch.sin(full_periods * 2 * torch.pi * x_vals)
+    noise = 0.1 * torch.randn(batch_size, seq_len, 1)
+    y_train = 2 * x_vals + 1 + noise
+
+    # Add noise features
+    x_train = torch.cat([x_vals, torch.randn(size=(batch_size, seq_len, n_features - 1))],
+        dim=2)
+
+    # Normalize
+    normalizer = Normalizer()
+    x_train = normalizer.normalize_x(x_train, training=True)
+    y_train = normalizer.normalize_y(y_train, training=True)
+
+    pretrain_model = Transformer('5k', x_train.size(2), normalizer=normalizer)
+    pretrain_model.train_model(
+        x_train,
+        y_train,
+        pretrain_now = True,
+        finetune_now = False,
+        verbose=2,
+        epochs=100, # deteiled pre-training
+        )
+
+    my_model = Transformer('5k', x_train.size(2), normalizer=normalizer)
+    my_model.train_model(
+        x_train,
+        y_train,
+        pretrain_now = False,
+        finetune_now = True,
+        verbose=2,
+        epochs=0, # skip fine-tuning
+        )
+
+    # Test input data: Sine Values
+    x_test_vals = torch.linspace(1, 2, batch_size * seq_len).reshape(batch_size, seq_len, 1)
+    x_test_vals = torch.sin(full_periods * 2 * torch.pi * x_test_vals)
+    y_test_true = 2 * x_test_vals + 1
+    x_test = torch.cat([x_test_vals, torch.randn(batch_size, seq_len,
+        n_features - 1)], dim=2)
+    x_test = normalizer.normalize_x(x_test, training=False)
+
+    # Normalize and predict
+    y_pred = my_model.predict(x_test)
+    y_pred = normalizer.de_normalize_y(y_pred)
+
+    # 1. Check output shape
+    assert y_pred.shape == (batch_size, seq_len, 1), \
+        f"Unexpected output shape: {y_pred.shape}"
+
+    # 2. Check relative accuracy
+    treshold = 20.0 # [%]
+    mae = torch.mean(torch.abs(y_pred - y_test_true))
+    mean_true = torch.mean(torch.abs(y_test_true))
+    n_mae = mae / mean_true
+    print(f"Relative error : {n_mae:.2%}")
+    assert n_mae < treshold/100., f"Model too inaccurate: {n_mae:.2%}"
+
 if __name__ == "__main__":
 
     for name, func in list(globals().items()):
